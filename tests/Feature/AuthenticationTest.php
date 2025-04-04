9cce48f4a694d7e6ef62c6acffd12721a2202e8f
Najibn <kawsunagib@gmail.com>
2025-04-04 15:55:40 +0200

    added request files,functional tests,refactored controller

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -3,6 +3,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -48,7 +49,7 @@ class AuthenticationTest extends TestCase
         ]);
     
         $response->assertStatus(200)
-        ->assertJson(['message' => 'logged in successfully']);
+        ->assertJson(['message' => 'Logged in successfully']);
         $this->assertAuthenticatedAs($user);
     }
 
@@ -64,8 +65,75 @@ class AuthenticationTest extends TestCase
     ]);
 
     $response->assertStatus(401)
-    ->assertJson(['message' => 'invalid entry']);
+    ->assertJson(['message' => 'Invalid Entry']);
     }
+
+
+    //user logout testting 
+    public function test_user_can_logout(){
+
+        //authenticat and create the user
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        //try to logout
+        $response = $this->postJson('/api/logout');
+
+        //we assert
+        $response -> assertStatus(200)->assertJson(['message' => 'successfully logged out']);
+        
+    }
+
+
+    public function test_requiring_all_user_fields(){
+
+        $response = $this->postJson('/api/register', []);
+
+        $response ->assertStatus(422)-> assertJsonValidationErrors([
+            'name', 'email', 'password', 'role'
+        ]);
+    }
+
+    //verifying users email must be unique 
+    public function test_user_email_uniqueness(){
+
+        //$this->withoutMiddleware(); 
+
+        User::factory()->create(['email'=> 'fakeemail@fske.com']);
+
+        $response = $this->postJson('/api/register', [
+
+            'name'                  => 'Test User',
+            'email'                 => 'fakeemail@fske.com', // the fake  email
+            'password'              => 'Password123!',
+            'password_confirmation' => 'Password123!',
+            'role'                  => 'customer'
+
+        ]);
+        $response -> assertStatus(422)->assertJsonValidationErrors(['email']);
+
+    }
+
+   //verifying user roles
+    public function test_role_must_be_valid_enum_value()
+    {
+
+    // adding invalid role check
+    $invalidData = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'Password123!',
+        'password_confirmation' => 'Password123!',
+        'role' => 'invalid_role_value'                  
+    ];
+    
+    $response = $this->postJson('/api/register', $invalidData);
+    
+    $response->assertStatus(422)     
+             ->assertJsonValidationErrors(['role']);
+    }
+
+
 
 }
 ?>
